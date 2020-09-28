7c2c5744377bedbfabab2685c199f4729d2ca95d
Gaurav <palande.gaurav777@gmail.com>
2020-09-28 12:57:37 +0530

    master => production.rb changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-
+  config.assets.compile = true
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
